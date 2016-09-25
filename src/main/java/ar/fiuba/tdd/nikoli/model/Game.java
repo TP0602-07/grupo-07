f225a60b5b1389bf7c495c42f64bea1b67bc717a
leandro <ltessore@github.com>
2016-09-25 00:38:34 -0300

    Agrego metodos que necesito de Game. Arreglo algunos errores de checkstyle

--- src/main/java/ar/fiuba/tdd/nikoli/model/Game.java
+++ src/main/java/ar/fiuba/tdd/nikoli/model/Game.java
@@ -1,6 +1,19 @@
 package ar.fiuba.tdd.nikoli.model;
 
+
+import ar.fiuba.tdd.nikoli.rules.Rule;
+
+import java.util.List;
+
 public class Game {
 
+    private List<Rule> conditions;
+    private String ui; //TODO Aca deberia ser la clase UI
+
+    //TODO
+    public void addCondition(Rule condition) {}
+
+    //TODO
+    public void setRules(String rules){}
 
 }
--- src/main/java/ar/fiuba/tdd/nikoli/model/builder/Builder.java
+++ src/main/java/ar/fiuba/tdd/nikoli/model/builder/Builder.java
@@ -2,22 +2,25 @@ package ar.fiuba.tdd.nikoli.model.builder;
 
 import ar.fiuba.tdd.nikoli.model.Game;
 import ar.fiuba.tdd.nikoli.rules.Rule;
-
-import java.util.List;
 import java.util.Iterator;
+import java.util.List;
 
 
-abstract class Builder {
+class Builder {
 
     protected Game game;
 
+    public Builder(Game game) {
+        this.game = game;
+    }
+
     public void createConditions(List<Rule> rules) {
         Iterator<Rule> rulesIterator = rules.iterator();
-        while (rulesIterator.hasNext()){
+        while (rulesIterator.hasNext()) {
             game.addCondition(rulesIterator.next());
         }
     }
     public void createRules(String rules) {
-        game.setRules(rules); //TODO
+        game.setRules(rules);
     }
 }
