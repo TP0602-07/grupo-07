7a4c65a74478b2e9aca2023b41fd743f4011ef3d
Pablo Ivancich <pivancich@velocitypartners.net>
2016-10-29 20:39:07 -0300

    Agregando JSONs con tableros y jugadas de tests.

--- src/test/java/ar/fiuba/tdd/nikoli/handlers/NikoliJsonHandlerTests.java
+++ src/test/java/ar/fiuba/tdd/nikoli/handlers/NikoliJsonHandlerTests.java
@@ -1,5 +1,42 @@
 package ar.fiuba.tdd.nikoli.handlers;
 
 
+import org.junit.Test;
+
+
 public class NikoliJsonHandlerTests {
+
+    private NikoliJsonHandler jsonHandler = new NikoliJsonHandler();
+
+    @Test
+    public void runGameInshiNoHeyaWin() {
+        String gameName = "inshinoheya-COMPLETE-WIN";
+        this.jsonHandler.runGame(gameName);
+    }
+
+    @Test
+    public void runGameInshiNoHeyaInvalidPlay() {
+        String gameName = "inshinoheya-INCOMPLETE-INVALID";
+        this.jsonHandler.runGame(gameName);
+    }
+
+
+    @Test
+    public void runGameCountryRoadWin() {
+        String gameName = "countryroad-COMPLETE-WIN";
+        this.jsonHandler.runGame(gameName);
+    }
+
+    @Test
+    public void runGameCountryRoadLose() {
+        String gameName = "countryroad-COMPLETE-LOSE";
+        this.jsonHandler.runGame(gameName);
+    }
+
+    @Test
+    public void runGameCountryRoadInvalidPlay() {
+        String gameName = "countryroad-INCOMPLETE-INVALID";
+        this.jsonHandler.runGame(gameName);
+    }
+
 }
